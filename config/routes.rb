45bffdb08b9cfdcdabcbaeaa1ad5d42967cecca8
kavya sudhakar <kavyasudhakar068@gmail.com>
2022-04-22 19:48:35 +0530

    designed login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+    resource :user
+
   get 'home/about'
   get 'home/lookup'
-  post "/home/lookup" => 'home#lookup'
+  get "home/lookup" => 'home#lookup'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
   # Defines the root path route ("/")
